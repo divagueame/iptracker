c8d396b243a24f8c95b19bc13e88994a9c03bfcb
martin <mocon@DESKTOP-1GQAV5L>
2021-09-26 20:15:55 +0200

    First map setup

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-// console.log("as")
 
-// let myAPI = get('https://geo.ipify.org/api/v1?apiKey=at_eFMjvs94s3EqPKEA1SL9fwvX7MhQA&ipAddress=8.8.8.8')
+$.getJSON('https://api.ipify.org?format=jsonp&callback=?', function(data) {
+  // console.log(JSON.stringify(data, null, 2));
+  let userIp = JSON.stringify(data, null, 2)
+  
+  // document.querySelector(".main").innerHTML = userIp
+
+  // var ip = "8.8.8.8";
+  var api_key = "at_eFMjvs94s3EqPKEA1SL9fwvX7MhQA";
+    $(function () {
+       $.ajax({
+           url: "https://geo.ipify.org/api/v1",
+           data: {apiKey: api_key},
+          //  data: {apiKey: api_key, ipAddress: ip},
+           success: function(data) {
+              // console.log(data)
+              //  $("body").append("<pre>"+ JSON.stringify(data,"",2)+"</pre>");
+           }
+       });
+    });
+
+
+
+  
+});
+
+
+    
+var mymap = L.map('mapid').setView([42.4299, 8.6446], 13);
