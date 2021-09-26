74a9cdb81b410ed9983d567699671aad17f20eb5
martin <mocon@DESKTOP-1GQAV5L>
2021-09-26 20:58:27 +0200

    Map relocating to starting point

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 $.getJSON('https://api.ipify.org?format=jsonp&callback=?', function(data) {
   // console.log(JSON.stringify(data, null, 2));
-  let userIp = JSON.stringify(data, null, 2)
+  // let userIp = JSON.stringify(data, null, 2)
   
   // document.querySelector(".main").innerHTML = userIp
 
@@ -13,25 +13,39 @@ $.getJSON('https://api.ipify.org?format=jsonp&callback=?', function(data) {
            data: {apiKey: api_key},
           //  data: {apiKey: api_key, ipAddress: ip},
            success: function(data) {
-              // console.log(data)
-               $("body").append("<pre>"+ JSON.stringify(data,"",2)+"</pre>");
+            // console.log(data.location['lat'])
+            // console.log(data.location['lng'])
+            let lat = data.location['lat'];
+            let lng = data.location['lng']
+            var map = L.map('mapid').setView([lat, lng],8);
+
+            L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+              attributionControl: false,
+            maxZoom: 18
+            })
+            .addTo(map);
+
+            // console.log(data.location)
+              //  $("body").append("<pre>"+ JSON.stringify(data,"",2)+"</pre>");
            }
        });
     });
-
-
-
   
 });
 
 
+// let api_key = 'at_eFMjvs94s3EqPKEA1SL9fwvX7MhQA';
+// // Make the request
+// fetch('https://geo.ipify.org/api/v1', {apiKey: api_key})
+//   // Extract JSON body content from HTTP response
+//   .then(response => response.json())
+//   // Do something with the JSON data
+//   .then(data => {
+//     console.log(JSON.stringify(data, null, 2))
+//   });
+
+
+
+
     
 // var mymap = L.map('mapid').setView([42.4299, 8.6446], 13);
-var map = L.map('mapid').
-setView([40.66, -5.72],
-12);
-
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-maxZoom: 18
-}).addTo(map);
