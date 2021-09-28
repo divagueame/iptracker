7e90045c63b8f525cf25f265df6730b25496f3c9
martin <mocon@DESKTOP-1GQAV5L>
2021-09-28 11:05:14 +0200

    Changed icon

--- index.js
+++ index.js
@@ -24,7 +24,19 @@ $.getJSON('https://api.ipify.org?format=jsonp&callback=?', function(data) {
             })
             .addTo(map);
 
-            var marker = L.marker([lat, lng]).addTo(map);
+
+            var greenIcon = L.icon({
+              iconUrl: './images/icon-location.svg',
+          
+              // iconSize:     [38, 95], // size of the icon
+              // shadowSize:   [50, 64], // size of the shadow
+              // iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+              // shadowAnchor: [4, 62],  // the same for the shadow
+              // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+          });
+
+
+            var marker = L.marker([lat, lng], {icon: greenIcon}).addTo(map);
 
             // console.log(data.location)
               //  $("body").append("<pre>"+ JSON.stringify(data,"",2)+"</pre>");
