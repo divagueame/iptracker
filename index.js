b395e35e6f343f2078e641055cb59a86df23fc4a
martin <mocon@DESKTOP-1GQAV5L>
2021-09-28 23:52:35 +0200

    Added relocation feature

--- index.js
+++ index.js
@@ -1,52 +1,108 @@
+mapInit()
 
-$.getJSON('https://api.ipify.org?format=jsonp&callback=?', function(data) {
-  // console.log(JSON.stringify(data, null, 2));
-  // let userIp = JSON.stringify(data, null, 2)
+function mapInit(){
   
-  // document.querySelector(".main").innerHTML = userIp
+  $.getJSON('https://api.ipify.org?format=jsonp&callback=?', function(data) {
+    var api_key = "at_eFMjvs94s3EqPKEA1SL9fwvX7MhQA";
+  
+      $(function () {
+         $.ajax({
+             url: "https://geo.ipify.org/api/v1",
+             data: {apiKey: api_key}, 
+             success: function(data) {
+              let lat = data.location['lat'];
+              let newLat = lat + 0.003
+              let lng = data.location['lng']
+              var map = L.map('mapid', {zoomControl: false });
+                
+              map.setView([newLat, lng],15);
 
-  // var ip = "8.8.8.8";
+              googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+              maxZoom: 20,
+              subdomains:['mt0','mt1','mt2','mt3']
+              })
+              .addTo(map);
+  
+              var userIcon = L.icon({
+                iconUrl: './images/icon-location.svg',
+                iconSize:     [46, 56]
+            });
+  
+            L.marker([lat, lng], {icon: userIcon}).addTo(map);
+
+
+              // This Information 
+              let thisIp = data.ip
+              let thisCity  = data.location.city
+              let thisPostalCode = data.location.postalCode
+              let thisLocation = `${thisCity}, ${thisPostalCode}`
+              let thisTimezone = data.location.timezone
+              let thisIsp = data.as.name
+  
+              newInfo = [thisIp,thisLocation,thisTimezone, thisIsp]
+              
+  
+              refreshInfo(newInfo)
+             }
+         });
+      });
+    
+  });
+  
+  
+  
+  
+  }
+
+
+// updateMap()
+
+function updateMap(ipInput){
+$.getJSON('https://api.ipify.org?format=jsonp&callback=?', function(data) {
   var api_key = "at_eFMjvs94s3EqPKEA1SL9fwvX7MhQA";
+
     $(function () {
        $.ajax({
            url: "https://geo.ipify.org/api/v1",
-           data: {apiKey: api_key},
+           data: {apiKey: api_key, ipAddress: (ipInput || null)}, 
            success: function(data) {
             let lat = data.location['lat'];
             let newLat = lat + 0.003
             let lng = data.location['lng']
-            var map = L.map('mapid', {zoomControl: false }).setView([newLat, lng],15);
+
+
+            var container = L.DomUtil.get('mapid');
+            if(container != null){
+              console.log("NOT EMPTY1")
+            container._leaflet_id = null;
+            }
+            
+            // console.log(document.getElementById('mapid'))
+            // let ref = document.getElementById('mapid')
+              // ref.innerHTML = `<div id="mapid"></div>`
+            
+              var map = L.map('mapid', {zoomControl: false })
+
+              // let container = map.getContainer()
+              // console.log(container)
+            // map.off()
+            // map.remove()
+            
+            
+            map.setView([newLat, lng],15);
             // This Information 
             let thisIp = data.ip
-
             let thisCity  = data.location.city
             let thisPostalCode = data.location.postalCode
             let thisLocation = `${thisCity}, ${thisPostalCode}`
-
             let thisTimezone = data.location.timezone
-
             let thisIsp = data.as.name
 
             newInfo = [thisIp,thisLocation,thisTimezone, thisIsp]
+            
 
-            function refreshInfo(newInfoArray){
-              
-              let layers = document.querySelectorAll('.overlay-info')
-              layers = Array.from(layers)
-              
-              layers.forEach((element,i) => {
-                element.innerHTML = newInfoArray[i]
-              });
-            }
             refreshInfo(newInfo)
-            // console.log(data.ip)
-            // console.log(data.location.city)
-            // console.log(data.location.postalCode)
-            // console.log(data.location.timezone)
-            // console.log(data.as.name)
-            // L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            // maxZoom: 18
-            // }).addTo(map);
+            // 
 
 
             googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
@@ -57,15 +113,12 @@ $.getJSON('https://api.ipify.org?format=jsonp&callback=?', function(data) {
 
             var userIcon = L.icon({
               iconUrl: './images/icon-location.svg',
-              iconSize:     [46, 56], // size of the icon
-              // shadowSize:   [50, 64], // size of the shadow
-              // iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-              // shadowAnchor: [4, 62],  // the same for the shadow
-              // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+              iconSize:     [46, 56]
           });
 
 
-            var marker = L.marker([lat, lng], {icon: userIcon}).addTo(map);
+            var marker = L.marker([lat, lng], {icon: userIcon})
+            .addTo(map);
 
             // console.log(data.location)
               //  $("body").append("<pre>"+ JSON.stringify(data,"",2)+"</pre>");
@@ -76,18 +129,28 @@ $.getJSON('https://api.ipify.org?format=jsonp&callback=?', function(data) {
 });
 
 
-// let api_key = 'at_eFMjvs94s3EqPKEA1SL9fwvX7MhQA';
-// // Make the request
-// fetch('https://geo.ipify.org/api/v1', {apiKey: api_key})
-//   // Extract JSON body content from HTTP response
-//   .then(response => response.json())
-//   // Do something with the JSON data
-//   .then(data => {
-//     console.log(JSON.stringify(data, null, 2))
-//   });
+
+
+}
 
 
 
+function refreshInfo(newInfoArray){
+              
+  let layers = document.querySelectorAll('.overlay-info')
+  layers = Array.from(layers)
+  layers.forEach((element,i) => {
+    element.innerHTML = newInfoArray[i]
+  });
+}
 
-    
-// var mymap = L.map('mapid').setView([42.4299, 8.6446], 13);
+
+
+let updateMapBtn = document.querySelector(".send-btn")
+let inputBox = document.getElementById("id_input")
+updateMapBtn.addEventListener('click', function(){
+  if(inputBox.value){
+    console.log(inputBox.value)
+    updateMap(inputBox.value)
+  }
+})
